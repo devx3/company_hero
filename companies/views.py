77753ad34b7a34bac243daecc610d804cec6ae26
DevX3 <erickfabiani123@gmail.com>
2020-10-02 15:57:08 -0300

    feat: Search user by Username OK

--- companies/views.py
+++ companies/views.py
@@ -21,3 +21,16 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def retrieve(self, request, pk):
+
+        try:
+            pk = int(pk)
+            queryset = Employee.objects.filter(id=pk)
+        except ValueError:
+            pk = str(pk)
+            queryset = Employee.objects.filter(
+                Q(username__icontains=pk)
+            )
+        serializer = EmployeeSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
